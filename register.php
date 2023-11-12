74b67b2f5614a0bf9df1e3570a8b95275e2ccde3
angad-0369 <76656446+angad-0369@users.noreply.github.com>
2023-11-13 02:18:10 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -1,96 +1,141 @@
 <?php
+require_once "config.php";
 
-  if(isset($_POST['s1'])){
-    $name = $_POST['name'];
-    $mail = $_POST['mail'];
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
 
-    $con = mysqli_connect("localhost","root","","responsibility");
-    if(!$con){
-        die("Could not connect!");
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Username cannot be blank";
+    }
+    else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        if($stmt)
+        {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set the value of param username
+            $param_username = trim($_POST['username']);
+
+            // Try to execute this statement
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    $username_err = "This username is already taken"; 
+                }
+                else{
+                    $username = trim($_POST['username']);
+                }
+            }
+            else{
+                echo "Something went wrong";
+            }
+        }
     }
 
-    $sql = "insert into signup (username,name,email,password) values ('$user','$name','$mail','$pass')";
+    mysqli_stmt_close($stmt);
 
-    if(mysqli_query($con,$sql)){
 
-      echo '<script>alert("USER SUCCESSFULLY REGISTERED !"); window.location.href = "login.php";</script>;';
+// Check for password
+if(empty(trim($_POST['password']))){
+    $password_err = "Password cannot be blank";
+}
+elseif(strlen(trim($_POST['password'])) < 5){
+    $password_err = "Password cannot be less than 5 characters";
+}
+else{
+    $password = trim($_POST['password']);
+}
+
+// Check for confirm password field
+if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
+    $password_err = "Passwords should match";
+}
+
+
+// If there were no errors, go ahead and insert into the database
+if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
+{
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    if ($stmt)
+    {
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+        // Set these parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Try to execute the query
+        if (mysqli_stmt_execute($stmt))
+        {
+            header("location: login.php");
+        }
+        else{
+            echo "Something went wrong... cannot redirect!";
+        }
     }
-
-    mysqli_close($con);
-  }
+    mysqli_stmt_close($stmt);
+}
+mysqli_close($conn);
+}
 
 ?>
+
 
 
 
 <!doctype html>
 <html lang="en">
   <head>
-
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/about.css">
-    <title>Responsibility Register</title>
-    <link rel="icon" type="image/x-icon" href="Images/favicon.webp">
-  <link rel="stylesheet" href="CSS/donateform.css" />
-  <style>
-    body {
-      background-color: #ecf4e9;
-      margin: 0px;
-    }
 
-    footer {
-      margin: 0;
-      width: 100%;
-      position: relative;
-      top: 220px;
-    }
-
-    .ww{
-      text-decoration: none;
-      font-weight: 600;
-    }
-
-    .d1{
-      margin-top: 100px;
-    }
-
-
-  </style>
+    <title>PHP login system!</title>
   </head>
   <body>
-  <header> 
-        <nav class="n">
-            <ul>
-                <span>
-                    <li><img src="Images/favicon.webp" alt=""></li>
-                    <li class="l1"><a href="index.html"> &nbsp;&nbsp;Responsibility</a></li>
-                    <div>
-                        <li><a class="ww" href="login.php">Login</a></li>
-                        <li><a class="ww" href="register.php">SignUp</a></li>
-                        
-                    </div>
-                    
-                </span>
-            </ul>
-        </nav> 
-    </header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Php Login System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
 
+      
+     
+    </ul>
+  </div>
+</nav>
 
 <div class="container mt-4">
-<h3 class="d1">Please Register Here:</h3>
+<h3>Please Register Here:</h3>
 <hr>
 <form action="" method="post">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Username</label>
-      <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Username">
+      <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Password</label>
@@ -102,15 +147,26 @@
       <input type="password" class="form-control" name ="confirm_password" id="inputPassword" placeholder="Confirm Password">
     </div>
   <div class="form-group">
-    <label for="inputAddress2">Name</label>
-    <input type="text" name="name" class="form-control" id="inputAddress2" placeholder="Enter Your Name">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Email</label>
-    <input type="text" class="form-control" id="inputAddress2" name="mail" placeholder="Enter Your Email ID">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
   </div>
   <div class="form-row">
-    
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
   <div class="form-group">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck">
@@ -119,7 +175,7 @@
       </label>
     </div>
   </div>
-  <button type="submit" name="s1" class="btn btn-primary" style="position:relative; left:40px;">Sign Up</button>
+  <button type="submit" class="btn btn-primary">Sign in</button>
 </form>
 </div>
 
@@ -128,36 +184,5 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-    <footer>
-    <div class="part4-1">
-      <img src="Images/favicon.webp" alt="">
-      <span>&nbsp;&nbsp;Responsibility</span>
-      <p>
-        Responsibility is the community-focused nonprofit donor management software built to deliver a better giving
-        experience and help organizations thrive.
-      </p>
-    </div>
-    <div class="part4-2">
-      <table border="0px" cellspacing="1px" align="right">
-        <tr class="td2">
-          <td><a href="#"><img style="padding-left:0px;" src="Images/l1.jpg" alt="Facebook"></a>
-            <a href="#"><img style="padding-left:0px;" src="Images/l2.png" alt="Twitter"></a>
-            <a href="#"><img style="padding-left:0px;" src="Images/l3.png" alt="Linked In">
-          </td>
-        </tr>
-        <tr class="td1">
-          <td><a href="#">Privacy Policy</a> <a href="#">Security Policy</a> <a href="#">Openness</a> <a
-              href="#">GDPR</a></td>
-        </tr>
-        <tr class="td3">
-          <td>Copyright © 2022 Responsibility® All Rights Reserved</td>
-        </tr>
-      </table>
-    </div>
-
-  </footer>
-
   </body>
 </html>
